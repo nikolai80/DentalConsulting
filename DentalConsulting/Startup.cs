c0d2ea3cff742c6ce55efcdc75a06f170d036b0e
unknown <D:\Lichnoe\TheBat>
2015-08-01 14:04:42 +0300

    Создание репозитория

---
+++ DentalConsulting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DentalConsulting.Startup))]
+namespace DentalConsulting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
