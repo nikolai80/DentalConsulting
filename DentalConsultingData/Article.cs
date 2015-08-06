94bec08f9f878a21ddcdb74a01e7dabb9567278f
unknown <D:\Lichnoe\TheBat>
2015-08-06 23:33:50 +0300

    Добавил диаграмму классов для сущностей

--- DentalConsultingData/Article.cs
+++ DentalConsultingData/Article.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,8 @@ namespace DentalConsultingData
 	{
 	class Article
 		{
+		public int ArticleId { get; set; }
+		public string ArticleTitle { get; set; }
+		public ArticleContent ArticleContent { get; set; }
 		}
 	}
