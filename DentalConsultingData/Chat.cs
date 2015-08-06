71b71dc072cfcec47e2aef0cbd9c8714c4e205a8
unknown <D:\TheBatBases>
2015-08-06 16:44:17 +0300

    добавил проект сущностей и классы сущностей

---
+++ DentalConsultingData/Chat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalConsultingData
+	{
+	/*Класс для чата между доктором и пользователем*/
+	class Chat
+		{
+		}
+	}
