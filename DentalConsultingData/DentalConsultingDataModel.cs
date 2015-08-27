7e6926ca482d418d532926966b6cabb00ab693de
nikolai80 <nicholasrabota@gmail.com>
2015-08-28 00:05:42 +0300

    добавил ADO.NET EDM

---
+++ DentalConsultingData/DentalConsultingDataModel.cs
@@ -0,0 +1,31 @@
+﻿namespace DentalConsultingData
+	{
+	using System;
+	using System.Data.Entity;
+	using System.Linq;
+
+	public class DentalConsultingDataModel : DbContext
+		{
+		// Контекст настроен для использования строки подключения "DentalConsultingDataModel" из файла конфигурации  
+		// приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+		// "DentalConsultingData.DentalConsultingDataModel" в экземпляре LocalDb. 
+		// 
+		// Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "DentalConsultingDataModel" 
+		// в файле конфигурации приложения.
+		public DentalConsultingDataModel()
+			: base("name=DentalConsultingDataModel")
+			{
+			}
+
+		// Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+		// о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+		// public virtual DbSet<MyEntity> MyEntities { get; set; }
+		}
+
+	//public class MyEntity
+	//{
+	//    public int Id { get; set; }
+	//    public string Name { get; set; }
+	//}
+	}
---
+++ DentalConsultingData/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DentalConsultingData
+	{
+
+	public class GenericRepository<TEntity> where TEntity : class
+		{
+		internal DentalConsultingContext context;
+	}
+}
