26ffd99b66ec4547872053e8811c0832c6b29681
nikolai80 <nicholasrabota@gmail.com>
2015-10-08 23:58:41 +0300

    улучшил верстку персональной страницы пациента добавил персональную страницу доктора

---
+++ DentalConsulting/Controllers/OwnDoctorPageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Mapping;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DentalConsulting.Controllers
+{
+    public class OwnDoctorPageController : Controller
+    {
+        // GET: OwnDoctorPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		//GET: DoctorArticles
+	    public ActionResult GetArticles()
+	    {
+		    return View();
+	    }
+    }
+}
