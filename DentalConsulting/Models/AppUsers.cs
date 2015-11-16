b2b97c884de38678b85d01c5f1ac6aafd20627f0
nikolai80 <nicholasrabota@gmail.com>
2015-11-16 21:12:32 +0300

    no message

--- DentalConsulting/Models/AppUsers.cs
+++ DentalConsulting/Models/AppUsers.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using DentalConsultingData;
+using DentalConsultingDAL;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace DentalConsulting.Models
@@ -12,5 +14,16 @@ namespace DentalConsulting.Models
 		public IEnumerable<ApplicationUser> IdentityAppUser { get; set; }
 		public IEnumerable<User> AppUser { get; set; }
 		public IEnumerable<IdentityRole> AppRoles { get; set; }
+
+		
+
+
+		public AppUsers()
+		{
+		var usersManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+		var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+		DentalConsultingDAL.IUser userRepository = new UserRepository(new DentalConsultingContext());
+		var appUsers = userRepository.GetUsers();//получили всех пользователей	
+		}
 		}
 	}
