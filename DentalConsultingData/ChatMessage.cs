234dc2e97242b8735e2e160972f48df0deac2ec7
nikolai80 <nicholasrabota@gmail.com>
2016-01-17 22:37:54 +0300

    Добавил класс сообщений чата

---
+++ DentalConsultingData/ChatMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DentalConsultingData
+	{
+
+	public class ChatMessage
+		{
+
+		public string User
+			{
+			get
+				{
+				throw new System.NotImplementedException();
+				}
+			set
+				{
+				}
+			}
+
+		public string Message
+			{
+			get
+				{
+				throw new System.NotImplementedException();
+				}
+			set
+				{
+				}
+			}
+	}
+}
